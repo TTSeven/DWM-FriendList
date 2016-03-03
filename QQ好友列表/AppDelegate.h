6323b4a724e30f15bfa71ac81eb4438b7ef8a6b4
TT <TT@126.com>
2016-03-03 16:52:44 +0800

    Initial Commit

---
+++ QQ好友列表/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ好友列表
+//
+//  Created by apple on 16/3/3.
+//  Copyright © 2016年 XiaoM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ好友列表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ好友列表
+//
+//  Created by apple on 16/3/3.
+//  Copyright © 2016年 XiaoM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
